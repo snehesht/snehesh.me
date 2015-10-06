7787db706594c50798e66000fbc7277c25137426
snehesht <snehesh85@gmail.com>
2015-10-05 21:34:27 -0400

    added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var minifyHTML = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+ 
+gulp.task('minify-css', function() {
+  return gulp.src('./src/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('minify-js', function() {
+  return gulp.src('./src/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build'));
+});
+ 
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+  return gulp.src('./src/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('build',['minify-css','minify-html','minify-js']);
